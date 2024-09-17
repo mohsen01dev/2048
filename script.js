653552567e6fb667f86a69a01fc706f9f326518a
mohsen01dev <mohsen01dev@gmail.com>
2024-09-17 04:59:05 +0330

    Write JavaScript code for left arrow key press
    - slide, merge left-matching tiles, and update row
    - handle key hold-down as continuous key press

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Define variables and DOM elements
 const gameBoardDiv = document.getElementById("game-board");
 let gameBoard;
+let isKeyDown = false;
 
 // Set up game when window loads
 window.onload = function () {
@@ -11,10 +12,10 @@ window.onload = function () {
 function setGame() {
   // Add initial game board with empty tiles
   gameBoard = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
+    [2, 2, 2, 2],
+    [2, 2, 2, 2],
+    [4, 4, 8, 8],
+    [4, 4, 8, 8],
   ];
 
   // Add and style tiles with IDs from game board
@@ -44,4 +45,55 @@ function updateTile(tile, num) {
     if (num <= 2048) tile.classList.add("t" + num.toString());
     else tile.classList.add("t-greater");
   }
+}
+
+// Process key press actions (once per key press)
+document.addEventListener("keydown", (e) => {
+  if (e.code == "ArrowLeft" && !isKeyDown) {
+    isKeyDown = true;
+    slideLeft();
+  }
+});
+
+// Reset key state on key release
+document.addEventListener("keyup", () => {
+  isKeyDown = false;
+});
+
+// Execute corresponding code if the pressed key is "left"
+function slideLeft() {
+  for (let r = 0; r < 4; r++) {
+    let row = gameBoard[r];
+    row = slide(row);
+    gameBoard[r] = row;
+
+    // Update DOM tiles
+    for (let c = 0; c < 4; c++) {
+      let tile = document.getElementById(r.toString() + "-" + c.toString());
+      let num = gameBoard[r][c];
+      updateTile(tile, num);
+    }
+  }
+}
+
+// Slide and merge left-matching tiles, then update row
+function slide(row) {
+  row = removeZero(row);
+
+  for (let i = 0; i < row.length - 1; i++) {
+    if (row[i] == row[i + 1]) {
+      row[i] *= 2;
+      row[i + 1] = 0;
+    }
+  }
+
+  row = removeZero(row);
+  while (row.length < 4) row.push(0); // Add zeros to end of array
+
+  return row;
+}
+
+// Create a new array without zeros
+function removeZero(row) {
+  return row.filter((num) => num != 0);
 }
